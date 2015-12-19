073cde8f2a0991e3002b2fef90fb1f00845529d4
Fabian Ehlert <fabian@fabianehlert.com>
2015-12-19 21:04:55 +0100

    Added FEQRScanViewController.swift

---
+++ FEQRScanViewController/FEQRScanViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FEQRScanViewController.swift
+//  FEQRScanViewController-Demo
+//
+//  Created by Fabian Ehlert on 19.12.15.
+//  Copyright © 2015 Fabian Ehlert. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class FEQRScanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
